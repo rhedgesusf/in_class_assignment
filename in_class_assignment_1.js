1e4e402bacc0c5d88c32634ed9a9f3f73fa7c5d9
Riley Hedges <rhedges1@usf.edu>
2025-01-30 16:17:16 -0500

    Passed Failed

--- in_class_assignment_1.js
+++ in_class_assignment_1.js
@@ -1,2 +1,20 @@
 // Task 1
 
+let x = 10;
+if (x > 5) {
+    console.log("Help");
+};
+
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses");
+} else {
+    console.log("Manageable Expenses");   
+};
+
+let score = 50;
+if (score >= 60) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+};
